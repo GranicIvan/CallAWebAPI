9eca4da99d279612944d61978fd6127c0fd91310
GranicIvan <granic.ivan.01@gmail.com>
2023-02-08 13:04:15 +0100

    Add project files.

---
+++ CallAWebAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("POCEKAT PROGRAMA!");
+
+
+
+
+        Console.WriteLine(" --- KRAJ PROGRAMA! ---");
+    }
+}
